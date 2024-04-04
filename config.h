298df025d781adc784b439d8e54385cf1d388678
DHSYan <sean.yan@student.unsw.edu.au>
2024-04-05 03:46:47 +1100

    slstatus and added rules or applications

--- config.h
+++ config.h
@@ -1,17 +1,17 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 7;        /* border pixel of windows */
+static const unsigned int borderpx  = 5;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const unsigned int systrayspacing = 3;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=18" };
-static const char dmenufont[]       = "monospace:size=18";
+static const char *fonts[]          = { "HurmitNerdFont:size=18" };
+static const char dmenufont[]       = "HurmitNerdFont:size=18";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#ff0000";
 static const char col_gray3[]       = "#bbbbbb";
@@ -38,7 +38,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "4", "5", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -47,7 +47,12 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+    { "Google",   NULL,       NULL,       1,       0,           -1 },
+    { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+    { "wezterm",  NULL,       NULL,       1 << 1,       0,           -1 },
+    { "Spotify",  NULL,       NULL,       1 << 5,       0,           -1 },
+    { "discord",  NULL,       NULL,       1 << 7,       0,           -1 },
+    { "obsidian", NULL,       NULL,       1 << 2,       0,           -1 },
 };
 
 /* layout(s) */
